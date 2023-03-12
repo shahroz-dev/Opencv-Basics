567facdb958466b980dbd974d64819d160f2586e
Muhammad Shahroz <shahrozarif28@gmail.com>
2023-03-12 21:55:37 +0500

    files

---
+++ chapter 7.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from helper import stackImages
+
+def empty(a):
+    pass
+
+
+path = "Resources/lambo.png"
+# Create a window of size 640 240 (width, height). In cv2 every function have (width, height) convention of input
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+# Add different trackbars on "TrackBars" windows
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)  # initial value is 0, max limit 179 (min limit is always
+# zero), empty() function receive the value
+cv2.createTrackbar("Hue Max", "TrackBars", 19, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 110, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 240, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 153, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # get all the realtime values from the trackbar
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+    #  get the filtered out image in these color range
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    # get the original image part using mask with AND operation
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+    # cv2.imshow("Original", img)
+    # cv2.imshow("HSV", imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", imgResult)
+
+    imgStack = stackImages(0.6, ([[img, imgHSV], [mask, imgResult]]))  # stack images with image scale 0.5.
+    cv2.imshow("Stack Images", imgStack)
+    cv2.waitKey(1)
